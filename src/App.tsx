cf8cf98d2c29a1f2e0b3f1cf559c9bcfb8682dee
Sarah Affonco <sarah.affonco@gmail.com.br>
2025-09-02 16:33:00 -0300

    criação e implementação de Tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Footer from './components/footer/Footer'
-import Navbar from './components/navbar/Navbar'
-import Cadastro from './pages/cadastro/Cadastro'
-import Home from './pages/home/Home'
-import Login from './pages/login/Login'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import ListaTemas from "./components/tema/listatemas/ListaTemas";
+import FormTema from "./components/tema/formtema/FormTema";
+import DeletarTema from "./components/tema/deletartema/DeletarTema";
 
 function App() {
-	return (
-		<>
-			<BrowserRouter>
-				<Navbar />
-				<div className="min-h-[80vh]">
-					<Routes>
-						<Route path="/" element={<Login />} />
-						<Route path="/home" element={<Home />} />
-						<Route path="/cadastro"	element={<Cadastro />}/>
-					</Routes>
-				</div>
-				<Footer />
-			</BrowserRouter>
-		</>
-	)
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/temas" element={<ListaTemas />} />
+            <Route path="/cadastrartema" element={<FormTema />} />
+            <Route path="/editartema/:id" element={<FormTema />} />
+			<Route path="/deletartema/:id" element={<DeletarTema />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
