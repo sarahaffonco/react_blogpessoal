f6f76972b64c246d383f34af5e95f101471b39d6
Sarah Affonco <sarah.affonco@gmail.com.br>
2025-09-05 10:35:41 -0300

    importação perfil

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import DeletarTema from "./components/tema/deletartema/DeletarTema";
 import ListaPostagens from "./components/postagem/listapostagens/ListaPostagens";
 import FormPostagem from "./components/postagem/formpostagem/FormPostagem";
 import DeletarPostagem from "./components/postagem/deletarpostagem/DeletarPostagem";
+import Perfil from "./pages/perfil/Perfil";
 
 function App() {
   return (
